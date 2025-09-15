0a536ab1b7507a2ef3b5e37ec8d3d77f9fea9e46
68mschmitt <68mschmitt@gmail.com>
2025-09-15 13:05:52 -0400

    Fix C-m

--- src/kilo.c
+++ src/kilo.c
@@ -17,7 +17,7 @@ void enableRawMode() {
     struct termios raw = orig_termios;
 
     tcgetattr(STDIN_FILENO, &raw);
-    raw.c_iflag &= ~(IXON);
+    raw.c_iflag &= ~(ICRNL | IXON);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
