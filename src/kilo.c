8cec9765f9a892f810410b241f3bbb580cce20c4
68mschmitt <68mschmitt@gmail.com>
2025-10-18 20:42:03 -0400

    Bind ctrl + q to quit

--- src/kilo.c
+++ src/kilo.c
@@ -8,6 +8,10 @@
 #include <termios.h>
 #include <unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)
+
 /*** data ***/
 
 struct termios orig_termios;
@@ -55,8 +59,7 @@ int main() {
         } else {
             printf("%d ('%c')\r\n", c, c);
         }
-
-        if (c == 'q') break;
+        if (c == CTRL_KEY('q')) break;
     }
     return 0;
 }
